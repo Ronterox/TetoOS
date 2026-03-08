44d1702959783b7acca83c2aaac0c9f9daf5bc06
Ricardo <ricarditomontserrat@gmail.com>
2026-03-08 21:32:48 +0100

    can print hex all types of it
    M  boot_sect_print_hex.asm
    M  bootloader.asm

--- boot_sect_print_hex.asm
+++ boot_sect_print_hex.asm
@@ -1,15 +1,46 @@
 ; receiving the data in 'dx'
 ; For the examples we'll assume that we're called with dx=0x1234
 print_hex:
+	pusha
+	mov cx, 0
 
 ; Strategy: get the last char of 'dx', then convert to ASCII
 ; Numeric ASCII values: '0' (ASCII 0x30) to '9' (0x39), so just add 0x30 to byte N.
 ; For alphabetic characters A-F: 'A' (ASCII 0x41) to 'F' (0x46) we'll add 0x40
 ; Then, move the ASCII byte to the correct position on the resulting string
 hex_loop:
+	cmp cx, 4
+	jge end_hex
+	inc cx
+
+	mov ax, bx
+	xor dx, dx
+
+	mov si, 0x10 ; 16 hex digits
+	div si ; divs ax, result on ax, rem in dx
+
+	mov bx, ax ; update new  0x1234 -> 0x123
+	add dx, '0' ; Turn into ascii
+
+	cmp dx, 0x39
+	jle step2
+
+	add dx, 0x7
 
 step2:
+	mov di, HEX_OUT
+	add di, 6
+	sub di, cx
+	mov [di], dl
 
-end:
+	jmp hex_loop
 
-HEX_OUT:
+end_hex:
+	mov bx, HEX_OUT
+	call print
+
+	popa
+	ret
+
+HEX_OUT db "0x0000", NL, 0
+
--- bootloader.asm
+++ bootloader.asm
@@ -3,12 +3,34 @@
 mov bp, 0x8000
 mov sp, bp
 
+mov bx, 0x1234
+call print_hex
+
+mov bx, 0x9876
+call print_hex
+
+mov bx, 0xABCD
+call print_hex
+
+mov bx, 0xdede
+call print_hex
+
+mov bx, 0xfafa
+call print_hex
+
+mov bx, 0x7c00
+call print_hex
+
+jmp $
+
 mov bx, MSG_REAL_MODE
 call print
 
 call switch_to_pm
 jmp $ ; just in case
 
+%define NL 0xA, 0xD ; \n, \r
+%include "boot_sect_print_hex.asm"
 %include "boot_sect_print.asm"
 %include "32bit-gdt.asm"
 %include "32bit-print.asm"
@@ -19,8 +41,6 @@ BEGIN_PM:
 	mov ebx, MSG_PROT_MODE
 	call print_pm
 	jmp $
-
-%define NL 0xA, 0xD ; \n, \r
 
 MSG_REAL_MODE db "Started in 16-bit real mode", NL, 0
 MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
